d5543fa7f4fe48aa3031b63c57ccf73e48a0d04d
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2022-03-02 14:00:43 +0700

    Tao luong bang Runnable

---
+++ src/RunnableDemo.java
@@ -0,0 +1,32 @@
+public class RunnableDemo implements Runnable {
+    private final String threadName;
+    private Thread t;
+
+    RunnableDemo(String name) {
+        threadName = name;
+        System.out.println("Creadting " + threadName);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Running " + threadName);
+        try {
+            for (int i = 4; i > 0; i--) {
+                System.out.println("Thread: " + threadName + ", " + i);
+                //  Dừng vòng lặp trong 50 ms
+                Thread.sleep(50);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread " + threadName + " interrupted.");
+        }
+        System.out.println("Thread " + threadName + " exitting.");
+    }
+
+    public void start() {
+        System.out.println("Starting " + threadName);
+        if (t == null) {
+            t = new Thread(this, threadName);
+            t.start();
+        }
+    }
+}
